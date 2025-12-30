55e1ddb33ecdbfa62143dfb3bdf4116874e5e0b6
kangansoo <ansoo971@gmail.com>
2025-12-30 17:02:51 +0900

    feat: 페이지 이동 시 스크롤 최상단 유지 기능 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import { StrictMode, Suspense } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import './i18n/i18n.ts';
+import { StrictMode, Suspense } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import { BrowserRouter } from "react-router-dom"
+import "./i18n/i18n.ts"
+import ScrollToTop from "./components/ScrollToTop.ts"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
+        <ScrollToTop />
         <App />
       </Suspense>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
